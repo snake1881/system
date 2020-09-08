3e08d505d81679b1140d934c73d7eaca5eb2e614
beiguoge <1776857122@qq.com>
2020-09-08 15:14:47 +0800

    change proxy

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,7 @@ proxyObj["/ws"] = {
 };
 proxyObj["/"] = {
   ws: false,
-<<<<<<< HEAD
-  target: "http://192.168.0.189:8692/demo",
-=======
   target: "http://192.168.0.106:8692",
->>>>>>> a5967d3b2211eb71f5b802b3bf57652d74b375a4
   changeOrigin: true,
   pathRewrite: {
     "^/": ""
