609be1232c21e12182c33ecb934dfd3a1cec0c0c
beiguoge <1776857122@qq.com>
2020-09-08 11:16:18 +0800

    change Login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,39 +1,47 @@
 <template>
   <div class="container">
-    <el-form
-      :model="sysUserLogin"
-      :rules="rules"
-      ref="loginForm"
-      label-width="20px"
-      class="loginContainer"
-    >
-      <h3 style="text-align: center;color: #505458;">管理系统</h3>
-      <el-form-item prop="username">
-        <el-input v-model="sysUserLogin.username" type="text" placeholder="用户名">
-          <i slot="prefix" class="el-input__icon el-icon-user" />
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input v-model="sysUserLogin.password" type="password" placeholder="密码">
-          <i slot="prefix" class="el-input__icon el-icon-goods" />
-        </el-input>
-      </el-form-item>
-      <el-link
-        type="primary"
-        style="margin:0 0 15px 320px;font-size:10px"
-        @click="goRegister"
+    <div class="container-left">
+      <img src="../assets/images/logo1.png" alt="加载中..." style="width: 30%; height: 25%;margin: 6% 35% 0 35%" />
+      <h3 style="text-align: center;color: #303133;font-size:32px;">定边采油厂 - 延长油田股份有限公司</h3>
+      <img src="../assets/images/chouyouji.png" alt="加载中..." style="width: 40%; height: 32%;margin: 9% 30% 0 30%" />
+    </div>
+    <div style="width: 28%; height: 100%;">
+      <img src="../assets/images/logo.png" alt="加载中..." style="width: 46%; height: 15%;margin: 10% 27% 2% 27%" />
+      <h3 style="text-align: center;color: #606266;font-size:20px;margin-bottom:70px">定边采油厂智能油田平台</h3>
+      <el-form
+        :model="sysUserLogin"
+        :rules="rules"
+        ref="loginForm"
+        label-width="20px"
+        class="loginContainer"
       >
-        注册
-      </el-link>
-      <el-button
-        size="small"
-        type="primary"
-        style="width: 100%;"
-        @click="submitLogin"
-      >
-        登录
-      </el-button>
-    </el-form>
+        <el-form-item prop="username">
+          <el-input v-model="sysUserLogin.username" type="text" placeholder="用户名">
+            <i slot="prefix" class="el-input__icon el-icon-user" />
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="sysUserLogin.password" type="password" placeholder="密码">
+            <i slot="prefix" class="el-input__icon el-icon-goods" />
+          </el-input>
+        </el-form-item>
+        <el-link
+          type="primary"
+          style="margin: 10px 25% 10px 65%;font-size:16px"
+          @click="goRegister"
+        >
+          注册
+        </el-link>
+        <el-button
+          size="small"
+          type="primary"
+          style="width: 50%;margin: 12px 25%;"
+          @click="submitLogin"
+        >
+          登录
+        </el-button>
+      </el-form>
+    </div>
   </div>
 </template>
 <script>
@@ -83,21 +91,34 @@ export default {
 
 <style lang="less" scoped>
 .container {
-  height: 100vh;
-  background-image: url("../assets/images/login.png");
-  background-size: 100% 100%;
-  background-repeat: no-repeat;
-  // margin: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
 }
-.loginContainer {
-  position: fixed;
-  border-radius: 15px;
-  background-clip: padding-box;
-  width: 350px;
-  margin: 200px 0px 0 1000px;
-  padding: 15px 35px 15px 35px;
-  background: #fff;
-  border: 1px solid #eaeaea;
-  box-shadow: 0 0 25px #cac6c6;
+.container-left {
+ width: 72%; 
+ height: 100%;
+ background: #9ADDFF;
+}
+</style>
+<style>
+.el-input__inner {
+  border-left-width:0px;
+  border-top-width:0px;
+  border-right-width:0px;
+  border-bottom-color:#909399;
+  border-radius: 0;
+  width: 60%;
+  margin: 10px 20%;
+}
+.el-input__prefix {
+  margin-left: 20%;
+}
+.el-form-item__error {
+  margin-left: 20%;
+}
+.el-button {
+  border-radius: 20px;
 }
 </style>
