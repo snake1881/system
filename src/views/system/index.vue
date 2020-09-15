550aecdf4c1ea8c8fa052b0cf8e7e259a462d2ab
beiguoge <1776857122@qq.com>
2020-09-15 13:45:20 +0800

    add system-index(router)

---
+++ src/views/system/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      系统管理首页
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style lang='less' scoped>
+</style>
+<style>
+</style>
