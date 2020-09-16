0c9ff95c384db45f6f75ee524b75f452fbb007b5
beiguoge <1776857122@qq.com>
2020-09-16 14:19:26 +0800

    add diagnosis

---
+++ src/views/diagnosis/diagnosis.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      油水井诊断
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style lang='less' scoped>
+</style>
+<style>
+</style>
