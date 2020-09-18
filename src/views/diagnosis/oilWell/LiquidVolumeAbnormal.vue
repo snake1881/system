8532a7c0322adf036b994c0198787e1a5e369f5b
beiguoge <1776857122@qq.com>
2020-09-18 16:08:41 +0800

    update liquidVolumeAbnormal

--- src/views/diagnosis/oilWell/LiquidVolumeAbnormal.vue
+++ src/views/diagnosis/oilWell/LiquidVolumeAbnormal.vue
@@ -3,21 +3,33 @@
     <!-- 条件查询 -->
     <el-form class="liquid_volume_abnormal_form" :model="abnormalForm" :inline="true">
       <el-form-item label="采油站">
-        <el-select v-model="abnormalForm.orgName" placeholder="全区">
+        <el-select v-model="abnormalForm.orgName" placeholder="全区" size="medium">
           <el-option label="徐梁采油站" value="徐梁采油站"></el-option>
           <el-option label="郑寨子采油站" value="郑寨子采油站"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-date-picker :v-model="abnormalForm.formDate" type="date" placeholder="选择日期" size="medium" />
+        <el-date-picker
+          :v-model="abnormalForm.formDate"
+          type="datetime"
+          placeholder="选择日期"
+          size="medium"
+        />
       </el-form-item>
       <el-form-item label="条件">
-        <el-select v-model="abnormalForm.liqOrWater" placeholder="全部">
+        <el-select v-model="abnormalForm.liqOrWater" placeholder="全部" size="medium">
           <el-option label="液量" value="shanghai"></el-option>
           <el-option label="含水" value="beijing"></el-option>
         </el-select>
       </el-form-item>
-      <el-button type="primary" icon="el-icon-search" size="small" @click="searchLiquidVolume()">查询</el-button>
+      <el-form-item>
+        <el-button
+          type="primary"
+          icon="el-icon-search"
+          size="small"
+          @click="searchLiquidVolume()"
+        >查询</el-button>
+      </el-form-item>
     </el-form>
     <!-- 表格数据 -->
     <el-table
@@ -25,30 +37,29 @@
       element-loading-text="拼命加载中"
       element-loading-spinner="el-icon-loading"
       :data="liquidVolumeAbnormal"
-      @selection-change="handleSelectionChange"
       height="84%"
       border
       style="width:100%;"
     >
-      <el-table-column prop="wellName" label="井号" width="120" />
+      <el-table-column prop="wellName" label="井号" width="100" />
       <el-table-column prop="prodDate" label="日期" width="160" />
-      <el-table-column prop="prodTime" label="生产时间" width="140" />
-      <el-table-column prop="abnormalProblem" label="诊断结果" width="140" />
-      <el-table-column prop="orgName" label="采油站" width="160" />
-      <el-table-column prop="liqProdDaily" label="产液量" width="140" />
-      <el-table-column prop="waterCut" label="含水率" width="130" />
-      <el-table-column prop="normalLiqProdDaily" label="正常产液量" width="140" />
-      <el-table-column prop="normalWaterCut" label="正常含水率" width="140" />
+      <el-table-column prop="prodTime" label="生产时间" width="80" />
+      <el-table-column prop="abnormalProblem" label="诊断结果" width="180" />
+      <el-table-column prop="orgName" label="采油站" width="140" />
+      <el-table-column prop="liqProdDaily" label="产液量" width="100" />
+      <el-table-column prop="waterCut" label="含水率" width="100" />
+      <el-table-column prop="normalLiqProdDaily" label="正常产液量" width="100" />
+      <el-table-column prop="normalWaterCut" label="正常含水率" width="100" />
       <el-table-column prop="remarks" label="备注" width="140" />
-      <el-table-column label="操作" width="100">
+      <el-table-column label="操作" width="120">
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="dleteLog(scope.row)">查看曲线</el-button>
-          <el-button type="text" size="small" @click="dleteLog(scope.row)">删除</el-button>
+          <el-button type="text" size="small" @click="details(scope.row)">查看曲线</el-button>
+          <el-button type="text" size="small" @click="dleteByPrimaryId(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 分页 -->
-    <div class="role_page">
+    <div class="liquid_volume_abnormal_page">
       <el-pagination
         :current-page.sync="currentPage"
         :page-size="pageSize"
@@ -59,6 +70,9 @@
         @current-change="handleCurrentChange"
       />
     </div>
+    <el-dialog title="详细信息" :visible.sync="dialogTableVisible">
+      
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -72,18 +86,18 @@ export default {
         // 采油站
         orgName: null,
         // 日期选择
-        formDate: null,
+        formDate: "",
         // 条件
         liqOrWater: null
       },
-      orgName: null,
-      date: null,
       // 分页数据
       currentPage: 1,
       pageSize: 10,
       total: 0,
       // 表格加载动画
-      loading: true
+      loading: true,
+      // 查看曲线对话框标记
+      dialogTableVisible: false
     };
   },
   created() {
@@ -96,11 +110,7 @@ export default {
         "/oilWell/liquidVolumeAbnormal/page?current=" +
           this.currentPage +
           "&pageSize=" +
-          this.pageSize +
-          "&orgName=" +
-          this.orgName +
-          "&date=" +
-          this.date
+          this.pageSize
       ).then(resp => {
         this.loading = false;
         if (resp) {
@@ -113,13 +123,21 @@ export default {
     },
     // 按条件查询
     searchLiquidVolume() {
-      this.getRequest(
-        "/oilWell/liquidVolumeAbnormal/page?current=" +
-          this.currentPage +
-          "&pageSize=" +
-          this.pageSize,
-        this.abnormalForm
-      ).then(resp => {
+      let url =
+        "/oilWell/liquidVolumeAbnormal/liquidVolumeAbnormal?current=" +
+        this.currentPage +
+        "&pageSize=" +
+        this.pageSize;
+      if (this.abnormalForm.orgName !== null) {
+        url += "&orgName=" + this.abnormalForm.orgName;
+      }
+      if (
+        this.abnormalForm.formDate !== null &&
+        this.abnormalForm.formDate !== ""
+      ) {
+        url += "&date=" + this.abnormalForm.formDate;
+      }
+      this.getRequest(url).then(resp => {
         this.loading = false;
         if (resp) {
           this.liquidVolumeAbnormal = resp.data.records;
@@ -128,6 +146,31 @@ export default {
           this.pageSize = resp.data.size;
         }
       });
+    },
+    // 查看曲线
+    details() {
+      this.dialogTableVisible = true;
+    },
+    // 根据primaryId删除异常数据
+    dleteByPrimaryId(val) {
+      let primaryId = val.primaryId;
+      this.deleteRequest(
+        "/oilWell/liquidVolumeAbnormal/liquidVolumeAbnormal/" + primaryId
+      ).then(resp => {
+        if (resp) {
+          this.liquidVolumeInit();
+        }
+      });
+    },
+    // 分页，页码大小改变
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.logInit();
+    },
+    // 分页，当前页改变
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      this.logInit();
     }
   }
 };
