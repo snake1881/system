6f2a4b5fecc97aed82cd42df8f59f813d804dfc6
lucky <luckymxp@163.com>
2020-09-29 09:00:55 +0800

    功图平铺

--- src/views/diagnosis/knowledge/tile.vue
+++ src/views/diagnosis/knowledge/tile.vue
@@ -5,10 +5,10 @@
       <el-form-item label="采油站:">
         <el-select v-model="logForm.oilStation" placeholder="全区">
           <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
+            v-for="item in oilStationOptions"
+            :key="item.orgName"
+            :label="item.orgName"
+            :value="item.orgName"
           ></el-option>
         </el-select>
       </el-form-item>
@@ -38,16 +38,31 @@
           size="medium"
         />
       </el-form-item>
-      <el-button type="primary" icon="el-icon-search" size="small" @click="searchTile()">查询</el-button>
+      <el-button
+        type="primary"
+        icon="el-icon-search"
+        size="small"
+        @click="searchTile()"
+        >查询</el-button
+      >
     </el-form>
     <div class="tile_echarts">
       <div
-        class="tile_echarts_child"
         v-for="item in tableData"
         :key="item.dynaId"
-        :id="item.dynaId"
-        :style="{width: '24%', height: '250px'}"
-      ></div>
+        :style="{ width: '25%', height: '260px' }"
+      >
+        <div
+          class="tile_echarts_child"
+          :key="item.dynaId"
+          :id="item.dynaId"
+        ></div>
+        <div class="tile_echarts_button">
+          <el-button type="primary" size="mini" @click="addTile(item)"
+            >添加</el-button
+          >
+        </div>
+      </div>
     </div>
     <!-- 分页 -->
     <div class="tile_page">
@@ -61,11 +76,21 @@
         @current-change="handleCurrentChange"
       />
     </div>
+    <!-- 新增 -->
+    <common-add-tile
+      :addTileVisible="addTileVisible"
+      :tileData="addTileData"
+      @tileRowClose="addTileClose"
+    />
   </div>
 </template>
 <script>
+import CommonAddTile from "../../../components/diagnosis/knowledge/CommonAddTile";
 let echarts = require("echarts/lib/echarts");
 export default {
+  components: {
+    CommonAddTile
+  },
   data() {
     return {
       //搜索框
@@ -78,7 +103,7 @@ export default {
         startTime: "2020-01-31",
         endTime: "2020-01-31"
       },
-
+      addTileVisible: false,
       options: [
         {
           value: "定1172-1",
@@ -101,6 +126,13 @@ export default {
           label: "定1174"
         }
       ],
+      oilStationOptions: [],
+      addTileData: {
+        wellName: "",
+        dynaCreateTime: "",
+        addType: "标准功图",
+        diagnosticResults: "泵工作正常"
+      },
       tableData: [],
       coordinates: [[]]
     };
@@ -109,18 +141,17 @@ export default {
   created() {
     this.searchTile();
     //输入框初始化默认时间
-    this.getNowTime();  
+    this.getNowTime();
+    this.queryOrgName();
   },
   methods: {
     //根据输入信息查询
     searchTile() {
-      console.log(this.logForm);
       this.postRequest(
         "/diagnosis/knowledge/tile/queryByterm",
         this.logForm
       ).then(resp => {
         if (resp) {
-          console.log(resp.data.records);
           this.tableData = resp.data.records;
           this.logForm.total = resp.data.total;
           this.logForm.currentPage = resp.data.current;
@@ -139,16 +170,24 @@ export default {
         }
       });
     },
+    //获取采油站信息
+    queryOrgName() {
+      this.getRequest("/diagnosis/knowledge/tile/queryorgName").then(resp => {
+        if (resp) {
+          this.oilStationOptions = resp.data;
+        }
+      });
+    },
     //设置初始化时间
     getNowTime() {
-      var now = new Date();
-      var year = now.getFullYear(); //得到年份
-      var month = now.getMonth(); //得到月份
-      var date = now.getDate(); //得到日期
-      month = month + 1;
-      month = month.toString().padStart(2, "0");
-      date = date.toString().padStart(2, "0");
-      var defaultDate = `${year}-${month}-${date}`;
+      // var now = new Date();
+      // var year = now.getFullYear(); //得到年份
+      // var month = now.getMonth(); //得到月份
+      // var date = now.getDate(); //得到日期
+      // month = month + 1;
+      // month = month.toString().padStart(2, "0");
+      // date = date.toString().padStart(2, "0");
+      // var defaultDate = `${year}-${month}-${date}`;
       // this.$set(this.logForm, "startTime", defaultDate);
       // this.$set(this.logForm, "endTime", defaultDate);
     },
@@ -202,7 +241,7 @@ export default {
           type: "value",
           axisLine: { onZero: false },
           nameTextStyle: {
-            padding: [8, 0, 0, 0],
+            padding: [10, 250, 0, 0],
             fontSize: 10
           }
         },
@@ -251,6 +290,17 @@ export default {
     handleCurrentChange(val) {
       this.logForm.currentPage = val;
       this.searchTile();
+    },
+    //添加功图至知识库
+    addTile(val) {
+      this.addTileData.wellName = val.wellId;
+      this.addTileData.dynaCreateTime = val.dynaCreateTime;
+      this.addTileVisible = true;
+      console.log(this.addTileData);
+    },
+    // 关闭新增对话框
+    addTileClose() {
+      this.addTileVisible = false;
     }
   }
 };
