879a912dfdf8c0a5f1311e98a07cf59b816aa92f
lucky <luckymxp@163.com>
2020-11-05 09:12:55 +0800

    知识库功图平铺样式调整

--- src/components/diagnosis/knowledge/CommonAddTile.vue
+++ src/components/diagnosis/knowledge/CommonAddTile.vue
@@ -8,19 +8,19 @@
     <div class="dialogDiv">
       <el-form :model="tileData" label-width="80px">
         <el-form-item label="井号">
-          <el-input v-model="tileData.wellName" />
+          <el-input v-model="tileData.wellId" />
         </el-form-item>
         <el-form-item label="采集时间">
           <el-input v-model="tileData.dynaCreateTime" />
         </el-form-item>
         <el-form-item label="添加成为">
-          <el-select v-model="tileData.addType" placeholder="请选择">
-            <el-option label="标准功图" value="标准功图"></el-option>
-            <el-option label="特征功图" value="特征功图"></el-option>
+          <el-select v-model="addType" placeholder="请选择">
+            <el-option label="标准功图" value="0"></el-option>
+            <el-option label="特征功图" value="1"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="特征结果">
-          <el-select v-model="tileData.diagnosticResults" placeholder="请选择">
+          <el-select v-model="diagnosticResults" placeholder="请选择">
             <el-option
               v-for="item in measuresoptions"
               :key="item.primaryId"
@@ -45,15 +45,31 @@ export default {
       type: Boolean
     },
     tileData: {
-      wellName: String,
-      dynaCreateTime: String,
-      addType: String,
-      diagnosticResults: String
+      type: Object
     }
   },
   data() {
     return {
-      measuresoptions: []
+      measuresoptions: [],
+      standardData: {
+        primaryId: "",
+        wellId: "",
+        wellCommonName: "",
+        checkDate: "",
+        dyna: "",
+        stroke: "",
+        frequency: "",
+        suspMaxLoad: "",
+        suspMinLoad: "",
+        upgoingMaxCurrent: "",
+        downgoingMaxCurrent: "",
+        label: "",
+        authDate: "",
+        author: "",
+        dynaId: ""
+      },
+      addType: "",
+      diagnosticResults: ""
     };
   },
   created() {
@@ -61,10 +77,39 @@ export default {
   },
   methods: {
     saveAddTile() {
+      // if (this.addType == 0) {
+      //   this.postRequest(
+      //     "/diagnosis/knowledge/standardCard/insert",
+      //     this.standardData
+      //   ).then(resp => {
+      //     if (resp) {
+      //       console.log(this.standardData);
+      //       this.$message({
+      //         message: "添加标准功图库成功!",
+      //         type: "success"
+      //       });
+      //     }else{
+      //       this.$message.error("添加标准功图库失败，请重新提交!");
+      //     }
+      //   });
+      // }
       console.log(this.tileData);
+      this.standardData.wellCommonName = this.tileData.wellId;
+      this.standardData.checkDate = this.tileData.dynaCreateTime;
+      this.standardData.stroke = this.tileData.stroke;
+      this.standardData.frequency = this.tileData.frequency;
+      this.standardData.suspMaxLoad = this.tileData.suspMaxLoad;
+      this.standardData.suspMinLoad = this.tileData.suspMinLoad;
+      this.standardData.upgoingMaxCurrent = this.tileData.upgoingMaxCurrent;
+      this.standardData.downgoingMaxCurrent = this.tileData.downgoingMaxCurrent;
+      this.standardData.label = this.diagnosticResults;
+      this.standardData.dynaId = this.tileData.dynaId;
+      console.log(this.standardData);
     },
     // 对话框父子组件传值
     addTileClose() {
+      this.addType = "";
+      this.diagnosticResults = "";
       this.$emit("tileRowClose");
     },
     //获取措施建议
