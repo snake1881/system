08b197f36b0e93b5717a147461d3092c95bf12e6
lucky <luckymxp@163.com>
2020-11-25 16:11:18 +0800

    井下作业查看数据填充

--- src/components/downholeOperations/CommonCheckOperation.vue
+++ src/components/downholeOperations/CommonCheckOperation.vue
@@ -10,26 +10,27 @@
         :active="checkData.nodeSequence"
         finish-status="success"
         class="checkOperDiv_top"
+        align-center
       >
         <el-step
           title="上报"
           @click.native="submit()"
-          description="2020-11-19，刘队"
+          :description="operAllNodeSource.submitTime + ' ' + operAllNodeSource.submitUserName"
         />
         <el-step
           title="派工"
           @click.native="send()"
-          description="2020-11-19，刘队"
+          :description="operAllNodeSource.sendTime + ' ' +operAllNodeSource.sendUserName"
         />
         <el-step
           title="现场作业"
           @click.native="scene()"
-          description="2020-11-19，刘队"
+          :description="operAllNodeSource.dispatchTime + ' ' +operAllNodeSource.dispatchUserName"
         />
         <el-step
           title="效果评价"
           @click.native="effect()"
-          description="2020-11-19，刘队"
+          :description="operAllNodeSource.measureTime + ' ' +operAllNodeSource.measureUserName"
         />
       </el-steps>
       <div class="checkOperDiv_bottom">
@@ -75,23 +76,23 @@
             </tr>
             <tr>
               <td rowspan="2">上次</td>
-              <td>作业名称:</td>
-              <td>作业队伍：</td>
+              <td>作业名称: {{ sendLastData.lastOperationName }}</td>
+              <td>作业队伍：{{ sendLastData.lastTeamName }}</td>
             </tr>
             <tr>
-              <td>作业时间:</td>
-              <td>距现在:</td>
+              <td>作业时间: {{ sendLastData.lastFinishDate }}</td>
+              <td>距现在: {{ sendLastData.dateCount }} 天</td>
             </tr>
             <tr>
               <td rowspan="4">本次</td>
-              <td>作业名称:</td>
-              <td>作业队伍：</td>
+              <td>作业名称: {{ sendNowData.lastOperationName }}</td>
+              <td>作业队伍：{{ sendNowData.lastTeamName }}</td>
             </tr>
             <tr>
-              <td colspan="2">派工单编号:</td>
+              <td colspan="2">派工单编号: {{ sendNowData.formNum }}</td>
             </tr>
             <tr>
-              <td colspan="2">备注:</td>
+              <td colspan="2">备注: {{ sendNowData.remark }}</td>
             </tr>
             <tr>
               <td colspan="2" style="height: 30%">附件:</td>
@@ -104,15 +105,22 @@
               <td colspan="4" style="text-align: center">现场作业</td>
             </tr>
             <tr>
-              <td>上报次数</td>
-              <td></td>
+              <td>上报次数:</td>
+              <td>
+                <a
+                  v-for="item in constructionData"
+                  :key="item.conspId"
+                  @click="constNum(item)"
+                  >{{ item.rownum }}
+                </a>
+              </td>
             </tr>
             <tr>
-              <td>上报日期</td>
-              <td>上报人:</td>
+              <td>上报日期: {{ constNumData.recordDate }}</td>
+              <td>上报人: {{ constNumData.recorder }}</td>
             </tr>
             <tr>
-              <td>进度描述</td>
+              <td>进度描述: {{ constNumData.progressDesc }}</td>
               <td></td>
             </tr>
             <tr>
@@ -131,16 +139,24 @@
               <td colspan="2" style="text-align: center">效果评价</td>
             </tr>
             <tr>
-              <td>评价结果</td>
-              <td>基本满足要求</td>
+              <td>评价结果:</td>
+              <td>
+                {{
+                  measureEffectData.evaluationResult == 0
+                    ? "失败"
+                    : measureEffectData.evaluationResult == 1
+                    ? "正常"
+                    : "较差"
+                }}
+              </td>
             </tr>
             <tr>
-              <td>评价日期</td>
-              <td>2020-11-19</td>
+              <td>评价日期:</td>
+              <td>{{ measureEffectData.evaluationDate }}</td>
             </tr>
             <tr>
-              <td>评价结论</td>
-              <td>挺好的</td>
+              <td>评价结论:</td>
+              <td>{{ measureEffectData.resultDesc }}</td>
             </tr>
           </table>
         </div>
@@ -160,12 +176,41 @@ export default {
     checkOperVisible: {
       type: Boolean,
     },
+    //各节点填报信息
+    operAllNodeSource: {
+      type: Object,
+    },
+    //当前作业信息
     checkData: {
       type: Object,
     },
+    //当前节点
     nodalPoint: {
       type: Number,
     },
+    //上次派工信息
+    sendLastData: {
+      type: Object,
+    },
+    //当前派工信息
+    sendNowData: {
+      type: Object,
+    },
+    //施工过程信息
+    constructionData: {
+      type: Object,
+    },
+    //某次施工信息
+    constNumData: {
+      type: Object,
+    },
+    //效果评价信息
+    measureEffectData: {
+      type: Object,
+    },
+  },
+  created() {
+    this.checkHistory();
   },
   data() {
     return {
@@ -182,6 +227,8 @@ export default {
       //如果当前节点处于上报节点或已完成上报，可以查看历史信息
       if (this.checkData.nodeSequence >= 0) {
         this.nodalPoint = 0;
+        //调用节点信息
+        this.checkHistory();
       } else {
         this.$message({
           message: "抱歉,不可以查看未执行任务!",
@@ -191,8 +238,9 @@ export default {
     },
     // 查看派工节点
     send() {
-      if (this.checkData.nodeSequence >= 0) {
+      if (this.checkData.nodeSequence >= 1) {
         this.nodalPoint = 1;
+        //调用节点信息
       } else {
         this.$message({
           message: "抱歉,不可以查看未执行任务!",
@@ -204,6 +252,8 @@ export default {
     scene() {
       if (this.checkData.nodeSequence >= 2) {
         this.nodalPoint = 2;
+        //调用节点信息
+        this.checkHistory();
       } else {
         this.$message({
           message: "抱歉,不可以查看未执行任务!",
@@ -215,12 +265,33 @@ export default {
     effect() {
       if (this.checkData.nodeSequence >= 3) {
         this.nodalPoint = 3;
+        //调用节点信息
+        this.checkHistory();
       } else {
         this.$message({
           message: "抱歉,不可以查看未执行任务!",
           type: "success",
         });
       }
+    },
+    // 获取某个节点历史信息
+    checkHistory() {
+      this.getRequest(
+        "/operation/operationInfo/queryNodeSourceByNode?nodeSequence=" +
+          this.nodalPoint +
+          "&wellOperationId=" +
+          this.checkData.wellOperationId
+      ).then((resp) => {
+        if (resp) {
+          //操作人信息填充
+          this.checkData.dealUserName = resp.data.dealUserName;
+          this.checkData.dealDate = resp.data.dealDate;
+        }
+      });
+    },
+    //获取某次进度信息
+    constNum(val) {
+      this.constNumData = val;
     },
   },
 };
