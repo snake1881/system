5ee7f090590050eac08d1476c9fa22fb507f6d50
lucky <luckymxp@163.com>
2020-11-27 09:24:12 +0800

    油井异常

--- src/views/diagnosis/abnormal/oilAbnormal.vue
+++ src/views/diagnosis/abnormal/oilAbnormal.vue
@@ -6,12 +6,19 @@
         <el-date-picker
           type="date"
           placeholder="选择日期"
-          v-model="postForm.date1"
-          style="width: 100%;"
+          v-model="postForm.postDate"
+          value-format="yyyy-MM-dd"
+          style="width: 100%"
         ></el-date-picker>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="small" @click="searchPost()">查询</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-search"
+          size="small"
+          @click="searchPost()"
+          >查询</el-button
+        >
       </el-form-item>
     </el-form>
     <!-- 表格数据 -->
@@ -19,23 +26,47 @@
       v-loading="loading"
       element-loading-text="拼命加载中"
       element-loading-spinner="el-icon-loading"
-      :data="postData"
+      :data="oilAbnormalData"
       height="84%"
       border
-      style="width:100%;"
+      style="width: 100%"
+      :row-style="{ height: '2px' }"
+      :cell-style="{ padding: '0px' }"
+      :header-cell-style="{ background: '#eef1f6', color: '#606266' }"
+      @cell-click="oilAbnormalCollect"
     >
-      <el-table-column prop="cyz" label="采油站" min-width="15%" />
-      <el-table-column prop="zjs" label="总井数" min-width="10%" />
-      <el-table-column prop="kjs" label="开井数" min-width="10%" />
-      <el-table-column prop="zcyl" label="总产液量" min-width="13%" />
-      <el-table-column prop="zcylz" label="总产液量(昨)" min-width="13%" />
-      <el-table-column prop="ylycs" label="液量异常数" min-width="10%" />
-      <el-table-column prop="hsycs" label="含水异常数" min-width="10%" />
-      <el-table-column prop="gkycs" label="工况异常数" min-width="10%" />
-      <el-table-column prop="dymycs" label="动液面异常数" min-width="10%" />
+      <el-table-column prop="oilStationName" label="采油站" min-width="15%" />
+      <el-table-column prop="sumWellCount" label="总井数" min-width="10%" />
+      <el-table-column prop="openWellCount" label="开井数" min-width="10%" />
+      <el-table-column prop="sumLiquid" label="总产液量" min-width="13%" />
+      <el-table-column
+        prop="sumLiquidYesterday"
+        label="总产液量(昨)"
+        min-width="13%"
+      />
+      <el-table-column
+        prop="liquidAbnormal"
+        label="液量异常数"
+        min-width="10%"
+      />
+      <el-table-column
+        prop="waterAbnormal"
+        label="含水异常数"
+        min-width="10%"
+      />
+      <el-table-column
+        prop="liquidWaterAbnormal"
+        label="工况异常数"
+        min-width="10%"
+      />
+      <el-table-column
+        prop="fluidAbnormal"
+        label="动液面异常数"
+        min-width="10%"
+      />
     </el-table>
-    <div id="wellChart" :style="{width: '98%', height: '300px'}"></div>
-    <div id="waterWellChart" :style="{width: '98%', height: '300px'}"></div>
+    <div id="wellChart" :style="{ width: '98%', height: '300px' }"></div>
+    <!-- <div id="waterWellChart" :style="{ width: '98%', height: '300px' }"></div> -->
   </div>
 </template>
 <script>
@@ -46,193 +77,89 @@ export default {
       //搜索框
       postForm: {
         positionName: "",
-        data1: ""
+        postDate: "",
       },
+      oilAbnormalData: [],
       // 表格数据
-      postData: [
-        {
-          cyz: "1766采油站",
-          zjs: "42",
-          kjs: "28",
-          zcyl: "235.43",
-          zcylz: "234.4",
-          ylycs: "0",
-          hsycs: "0",
-          gkycs: "1",
-          dymycs: "2"
-        },
-        {
-          cyz: "一号采油站",
-          zjs: "63",
-          kjs: "35",
-          zcyl: "278.55",
-          zcylz: "276.89",
-          ylycs: "0",
-          hsycs: "0",
-          gkycs: "23",
-          dymycs: "3"
-        },
-        {
-          cyz: "上菜渠采油站",
-          zjs: "54",
-          kjs: "11",
-          zcyl: "259.43",
-          zcylz: "231.4",
-          ylycs: "14",
-          hsycs: "0",
-          gkycs: "0",
-          dymycs: "1"
-        },
-        {
-          cyz: "下菜渠采油站",
-          zjs: "54",
-          kjs: "11",
-          zcyl: "259.43",
-          zcylz: "231.4",
-          ylycs: "14",
-          hsycs: "0",
-          gkycs: "0",
-          dymycs: "1"
-        },
-        {
-          cyz: "东关增压站",
-          zjs: "64",
-          kjs: "35",
-          zcyl: "259.43",
-          zcylz: "208.4",
-          ylycs: "11",
-          hsycs: "0",
-          gkycs: "0",
-          dymycs: "2"
-        },
-        {
-          cyz: "东关采油站",
-          zjs: "54",
-          kjs: "11",
-          zcyl: "253.43",
-          zcylz: "281.4",
-          ylycs: "18",
-          hsycs: "3",
-          gkycs: "0",
-          dymycs: "1"
-        },
-        {
-          cyz: "庙沟采油站",
-          zjs: "54",
-          kjs: "11",
-          zcyl: "229.43",
-          zcylz: "271.4",
-          ylycs: "17",
-          hsycs: "0",
-          gkycs: "0",
-          dymycs: "1"
-        },
-        {
-          cyz: "张天渠采油站",
-          zjs: "54",
-          kjs: "11",
-          zcyl: "259.43",
-          zcylz: "231.4",
-          ylycs: "14",
-          hsycs: "0",
-          gkycs: "0",
-          dymycs: "1"
-        }
-      ]
+      wellChartData: [[]],
+      // 表格加载动画
+      loading: true,
     };
   },
   mounted() {
-    //实例化echarts
-    this.drawLine();
+    //表格初始化
+    this.postInit();
   },
   created() {},
   methods: {
     // 画图
-    drawLine() {
+    drawLine(val) {
       // 基于准备好的dom，初始化echarts实例
       let dom = document.getElementById("wellChart");
-      let dom2 = document.getElementById("waterWellChart");
+      // let dom2 = document.getElementById("waterWellChart");
       let myChart = echarts.init(dom);
-      let myChart2 = echarts.init(dom2);
+      // let myChart2 = echarts.init(dom2);
       // 绘制图表
       myChart.setOption({
         title: {
           x: "center",
-          text: "采油井异常信息汇总"
+          text: "采油井异常信息汇总",
         },
         tooltip: {
           trigger: "axis",
           axisPointer: {
             // 坐标轴指示器，坐标轴触发有效
-            type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+            type: "shadow", // 默认为直线，可选为：'line' | 'shadow'
           },
-          formatter: "{c}"
+          formatter: "{c}",
         },
         legend: {
-          top: "10%"
+          top: "10%",
         },
         grid: {
           left: "3%",
           right: "3%",
           bottom: "15%",
           top: "20%",
-          containLabel: true
+          containLabel: true,
         },
         dataset: {
-          source: [
-            [
-              "carType",
-              "总井数",
-              "开井数",
-              "总产液量",
-              "总产液量(昨)",
-              "液量异常数",
-              "含水异常数",
-              "工况异常数",
-              "设备异常数"
-            ],
-            ["1766采油站", 23, 28, 168.5, 175.5, 0, 0, 1, 2],
-            ["一号采油站", 15, 11, 99.5, 109.9, 12, 7, 0, 1],
-            ["上菜渠采油站", 20, 16, 154.5, 198.8, 19, 0, 0, 1],
-            ["下菜渠采油站", 13, 8, 202.5, 217.8, 16, 0, 2, 1],
-            ["庙沟采油站", 53, 11, 162.5, 165.4, 17, 0, 0, 1],
-            ["张天渠采油站", 54, 11, 134.5, 145.43, 14, 0, 0, 1]
-          ]
+          source: val,
         },
         xAxis: {
           type: "category",
           axisTick: {
-            show: false //隐藏x坐标轴刻度
+            show: false, //隐藏x坐标轴刻度
           },
           //x轴字体样式
           axisLabel: {
             show: true,
             textStyle: {
               color: "#42436A",
-              fontSize: "12"
+              fontSize: "12",
             },
-            rotate: "30" //字体倾斜角度
+            rotate: "30", //字体倾斜角度
           },
           //x轴颜色
           axisLine: {
             lineStyle: {
-              color: "#e4e4e4"
-            }
-          }
+              color: "#e4e4e4",
+            },
+          },
         },
         yAxis: {
           type: "value",
           minInterval: 0, //只显示整数
           axisLine: {
-            show: false //隐藏y坐标轴
+            show: false, //隐藏y坐标轴
           },
           //y轴字体样式
           axisLabel: {
             show: true,
             textStyle: {
               color: "#9495ab",
-              fontSize: "12"
-            }
+              fontSize: "12",
+            },
           },
           //网格样式
           splitLine: {
@@ -240,12 +167,12 @@ export default {
             lineStyle: {
               color: "#e4e4e4",
               width: 1,
-              type: "solid"
-            }
+              type: "solid",
+            },
           },
           axisTick: {
-            show: false //隐藏y坐标轴刻度
-          }
+            show: false, //隐藏y坐标轴刻度
+          },
         },
         series: [
           {
@@ -255,195 +182,9 @@ export default {
             itemStyle: {
               normal: {
                 //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          },
-          {
-            name: "开井数",
-            type: "bar",
-            barWidth: 6, //柱体宽度
-            itemStyle: {
-              normal: {
-                //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          },
-          ,
-          {
-            name: "总产液量",
-            type: "bar",
-            barWidth: 6, //柱体宽度
-            itemStyle: {
-              normal: {
-                //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          },
-          ,
-          {
-            name: "总产液量(昨)",
-            type: "bar",
-            barWidth: 6, //柱体宽度
-            itemStyle: {
-              normal: {
-                //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          },
-          {
-            name: "液量异常数",
-            type: "bar",
-            barWidth: 6, //柱体宽度
-            itemStyle: {
-              normal: {
-                //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          },
-          {
-            name: "含水异常数",
-            type: "bar",
-            barWidth: 6, //柱体宽度
-            itemStyle: {
-              normal: {
-                //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          },
-          {
-            name: "工况异常数",
-            type: "bar",
-            barWidth: 6, //柱体宽度
-            itemStyle: {
-              normal: {
-                //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          },
-          {
-            name: "设备异常数",
-            type: "bar",
-            barWidth: 6, //柱体宽度
-            itemStyle: {
-              normal: {
-                //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          }
-        ]
-      });
-      // 绘制图表
-      myChart2.setOption({
-        title: {
-          x: "center",
-          text: "注水井异常信息汇总"
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            // 坐标轴指示器，坐标轴触发有效
-            type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
-          },
-          formatter: "{c}"
-        },
-        legend: {
-          top: "10%"
-        },
-        grid: {
-          left: "3%",
-          right: "3%",
-          bottom: "15%",
-          top: "20%",
-          containLabel: true
-        },
-        dataset: {
-          source: [
-            [
-              "carType",
-              "总井数",
-              "开井数",
-              "总产液量",
-              "总产液量(昨)",
-              "液量异常数",
-              "含水异常数",
-              "工况异常数",
-              "设备异常数"
-            ],
-            ["1766采油站", 23, 28, 168.5, 175.5, 0, 0, 1, 2],
-            ["一号采油站", 15, 11, 99.5, 109.9, 12, 7, 0, 1],
-            ["上菜渠采油站", 20, 16, 154.5, 198.8, 19, 0, 0, 1],
-            ["下菜渠采油站", 13, 8, 202.5, 217.8, 16, 0, 2, 1],
-            ["庙沟采油站", 53, 11, 162.5, 165.4, 17, 0, 0, 1],
-            ["张天渠采油站", 54, 11, 134.5, 145.43, 14, 0, 0, 1]
-          ]
-        },
-        xAxis: {
-          type: "category",
-          axisTick: {
-            show: false //隐藏x坐标轴刻度
-          },
-          //x轴字体样式
-          axisLabel: {
-            show: true,
-            textStyle: {
-              color: "#42436A",
-              fontSize: "12"
+                barBorderRadius: [10, 10, 0, 0],
+              },
             },
-            rotate: "30" //字体倾斜角度
-          },
-          //x轴颜色
-          axisLine: {
-            lineStyle: {
-              color: "#e4e4e4"
-            }
-          }
-        },
-        yAxis: {
-          type: "value",
-          minInterval: 0, //只显示整数
-          axisLine: {
-            show: false //隐藏y坐标轴
-          },
-          //y轴字体样式
-          axisLabel: {
-            show: true,
-            textStyle: {
-              color: "#9495ab",
-              fontSize: "12"
-            }
-          },
-          //网格样式
-          splitLine: {
-            show: true,
-            lineStyle: {
-              color: "#e4e4e4",
-              width: 1,
-              type: "solid"
-            }
-          },
-          axisTick: {
-            show: false //隐藏y坐标轴刻度
-          }
-        },
-        series: [
-          {
-            name: "总井数",
-            type: "bar",
-            barWidth: 6, //柱体宽带
-            itemStyle: {
-              normal: {
-                //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
           },
           {
             name: "开井数",
@@ -452,11 +193,10 @@ export default {
             itemStyle: {
               normal: {
                 //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
+                barBorderRadius: [10, 10, 0, 0],
+              },
+            },
           },
-          ,
           {
             name: "总产液量",
             type: "bar",
@@ -464,11 +204,10 @@ export default {
             itemStyle: {
               normal: {
                 //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
+                barBorderRadius: [10, 10, 0, 0],
+              },
+            },
           },
-          ,
           {
             name: "总产液量(昨)",
             type: "bar",
@@ -476,9 +215,9 @@ export default {
             itemStyle: {
               normal: {
                 //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
+                barBorderRadius: [10, 10, 0, 0],
+              },
+            },
           },
           {
             name: "液量异常数",
@@ -487,9 +226,9 @@ export default {
             itemStyle: {
               normal: {
                 //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
+                barBorderRadius: [10, 10, 0, 0],
+              },
+            },
           },
           {
             name: "含水异常数",
@@ -498,9 +237,9 @@ export default {
             itemStyle: {
               normal: {
                 //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
+                barBorderRadius: [10, 10, 0, 0],
+              },
+            },
           },
           {
             name: "工况异常数",
@@ -509,29 +248,74 @@ export default {
             itemStyle: {
               normal: {
                 //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
+                barBorderRadius: [10, 10, 0, 0],
+              },
+            },
           },
           {
-            name: "设备异常数",
+            name: "动液面异常数",
             type: "bar",
             barWidth: 6, //柱体宽度
             itemStyle: {
               normal: {
                 //柱体圆角
-                barBorderRadius: [10, 10, 0, 0]
-              }
-            }
-          }
-        ]
+                barBorderRadius: [10, 10, 0, 0],
+              },
+            },
+          },
+        ],
       });
     },
     // 根据输入信息查询
-    searchPost() {},
+    searchPost() {
+      this.postInit();
+    },
     //表格数据初始化
-    postInit() {}
-  }
+    postInit() {
+      this.getRequest(
+        "/diagnosis/abnormal/queryOilProductionAbnormal?createTime=" +
+          this.postForm.postDate
+      ).then((resp) => {
+        this.loading = false;
+        if (resp) {
+          this.oilAbnormalData = resp.data;
+           console.log(this.oilAbnormalData);
+          //处理数据为坐标
+          this.coordinate(resp.data);
+          //延迟到DOM更新之后再执行绘制图形
+          this.$nextTick(function () {
+            //实例化echarts
+            this.drawLine(this.coordinates);
+          });
+        }
+      });
+    },
+    //将坐标数据串处理为坐标点
+    coordinate(val) {
+      //每次处理之前保证坐标数组集合为空
+      this.coordinates = [[]];
+      for (var i = 0; i < val.length; i++) {
+        var array = [];
+        array[0] = val[i].oilStationName;
+        array[1] = val[i].sumWellCount;
+        array[2] = val[i].openWellCount;
+        array[3] = val[i].sumLiquid;
+        array[4] = val[i].sumLiquidYesterday;
+        array[5] = val[i].liquidAbnormal;
+        array[6] = val[i].waterAbnormal;
+        array[7] = val[i].liquidWaterAbnormal;
+        array[8] = val[i].fluidAbnormal;
+        this.coordinates[i] = array;
+      }
+      console.log(this.coordinates);
+      return this.coordinates;
+    },
+    //液量异常详情
+    oilAbnormalCollect(row, column) {
+      console.log(row);
+      console.log(column);
+    },
+  },
 };
 </script>
 <style lang="less" scoped>
