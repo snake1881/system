4db07a962cd2fcf9e8d1a9ec0ccc47b3e121c093
cwqz <xiaonifan@163.com>
2020-11-27 09:23:17 +0800

    井下作业查看页面修改

--- src/components/downholeOperations/CommonSendOperation.vue
+++ src/components/downholeOperations/CommonSendOperation.vue
@@ -2,59 +2,65 @@
   <el-dialog
     :title="sendData.wellId + '井 井下作业派工'"
     :visible.sync="sendOperVisible"
-    width="55%"
+    width="60%"
     :before-close="sendOperClose"
   >
     <div class="sendOpereDiv">
-      <el-form :model="dispathchInfoData" label-width="100px">
+      <el-form :model="dispathchInfoData" label-width="75px">
         <el-row>
-          <el-col :span="8">
+          <el-col :span="6">
             <el-form-item label="上次作业:">
               <el-input v-model="sendLastData.lastOperationName" />
             </el-form-item>
           </el-col>
-          <el-col :span="8">
-            <el-form-item label="上次作业队伍:">
+          <el-col :span="6">
+            <el-form-item label="上次队伍:">
               <el-input v-model="sendLastData.lastTeamName" />
             </el-form-item>
           </el-col>
-          <el-col :span="8">
-            <el-form-item label="上次作业时间:">
+          <el-col :span="6">
+            <el-form-item label="上次时间:">
               <el-input v-model="sendLastData.lastFinishDate" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="距现在:">
+              <el-input v-model="sendLastData.dateCount" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
-          <el-col :span="8">
-            <el-form-item label="距现在天数:">
-              <el-input v-model="sendLastData.dateCount" />
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
+          <el-col :span="6">
             <el-form-item label="本次作业:">
               <el-input v-model="sendData.operationName" />
             </el-form-item>
           </el-col>
-          <el-col :span="8">
-            <el-form-item label="派工单编号:">
+          <el-col :span="6">
+            <el-form-item label="派工单号:">
               <el-input v-model="dispathchInfoData.formNum" />
             </el-form-item>
           </el-col>
+          <el-col :span="6">
+            <el-form-item label="作业队伍:">
+              <el-select
+                v-model="dispathchInfoData.teamId"
+                placeholder="请选择"
+              >
+                <el-option
+                  v-for="item in teamNameoptions"
+                  :key="item.teamName"
+                  :label="item.teamName"
+                  :value="item.teamId"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="备注:">
+              <el-input v-model="dispathchInfoData.remark" />
+            </el-form-item>
+          </el-col>
         </el-row>
-        <el-form-item label="作业队伍:">
-          <el-select v-model="dispathchInfoData.teamId" placeholder="请选择">
-            <el-option
-              v-for="item in teamNameoptions"
-              :key="item.teamName"
-              :label="item.teamName"
-              :value="item.teamId"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-
-        <el-form-item label="备注:">
-          <el-input v-model="dispathchInfoData.remark" type="textarea" />
-        </el-form-item>
         <el-form-item label="附件:">
           <el-upload class="upload-demo" drag action="" multiple>
             <i class="el-icon-upload" />
@@ -165,8 +171,9 @@ export default {
   overflow: auto;
 }
 
-.sendOpereDiv .el-input {
-  width: 150px;
+.sendOpereDiv .el-input,
+.el-select {
+  width: 140px;
   height: 2px;
 }
 
