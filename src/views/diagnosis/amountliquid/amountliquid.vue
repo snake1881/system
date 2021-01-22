c202b847ee069106cafea388001491c574571dd6
yezhiying <18451875727@163.com>
2021-01-22 18:00:09 +0800

    智能量液导出添加字段

--- src/views/diagnosis/amountliquid/amountliquid.vue
+++ src/views/diagnosis/amountliquid/amountliquid.vue
@@ -266,6 +266,7 @@ export default {
         "最小位移",
         "产液量",
         "日累产液",
+        "24小时产液量",
       ];
       const filterVal = [
         "wellName",
@@ -279,6 +280,7 @@ export default {
         "minDisplacement",
         "hourProd",
         "dayProd",
+        "liquidProd",
       ];
       const list = this.selectData;
       const data = this.formatJson(filterVal, list);
