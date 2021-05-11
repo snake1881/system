e5d988c1f1ffc3c6279c6160b321c86d3ba649f8
cwqz <xiaonifan@163.com>
2021-05-11 11:53:03 +0800

    系统概况页样式修改

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -1,17 +1,116 @@
 <template>
   <div class="survey">
-    <el-button
-      @click="gotoIndex()"
-      class="survey_footer"
-      type="text"
-      icon="el-icon-video-play"
-      size="medium "
-      >进入系统</el-button
-    >
+    <div class="survey_header">
+      <el-button
+        @click="gotoIndex()"
+        class="survey_header_button"
+        type="text"
+        icon="el-icon-right"
+        >进入系统</el-button
+      >
+    </div>
+    <div class="survey_content">
+      <div class="survey_content_left">
+        <el-card>
+          <div slot="header">
+            <span class="card_header">油水井诊断</span>
+          </div>
+          <img src="../assets/images/survey_one.png" class="card_left_image" />
+        </el-card>
+        <el-card>
+          <div slot="header">
+            <span class="card_header">无人值守</span>
+          </div>
+          <img src="../assets/images/survey_two.png" class="card_left_image" />
+        </el-card>
+        <el-card>
+          <div slot="header">
+            <span class="card_header">井下作业</span>
+          </div>
+          <img
+            src="../assets/images/survey_three.png"
+            class="card_left_image"
+          />
+        </el-card>
+      </div>
+      <div class="survey_content_middle">
+        <div class="survey_content_middle_map">
+          <img
+            src="../assets/images/block.png"
+            style="width: 96%; height: 94%; margin: 2% 2%"
+          />
+          <!-- <el-amap
+            vid="amapPro"
+            :amap-manager="amapManager"
+            :mapStyle="mapStyle"
+            :zoom="zoom"
+            :center="center"
+            :events="events"
+          /> -->
+        </div>
+        <div class="survey_content_middle_body">
+          <el-card>
+            <div slot="header">
+              <span class="card_header">含水监测分析</span>
+            </div>
+            <img src="../assets/images/survey_four.png" class="card_image" />
+          </el-card>
+          <el-card>
+            <div slot="header">
+              <span class="card_header">注水井报表</span>
+            </div>
+            <img src="../assets/images/survey_five.png" class="card_image" />
+          </el-card>
+          <el-card>
+            <div slot="header">
+              <span class="card_header">视频监控</span>
+            </div>
+            <img src="../assets/images/survey_six.png" class="card_image" />
+          </el-card>
+        </div>
+      </div>
+      <div class="survey_content_left">
+        <el-card>
+          <div slot="header">
+            <span class="card_header">绩效考核</span>
+          </div>
+          <img src="../assets/images/survey_nine.png" class="card_left_image" />
+        </el-card>
+        <el-card>
+          <div slot="header">
+            <span class="card_header">系统管理</span>
+          </div>
+          <img
+            src="../assets/images/survey_eight.png"
+            class="card_left_image"
+          />
+        </el-card>
+        <el-card>
+          <div slot="header">
+            <span class="card_header">基础信息</span>
+          </div>
+          <img
+            src="../assets/images/survey_seven.png"
+            class="card_left_image"
+          />
+        </el-card>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+// import { AMapManager } from "vue-amap";
+// let amapManager = new AMapManager(); // 新建生成地图画布
 export default {
+  data() {
+    return {
+      //   amapManager,
+      //   zoom: 12, // 设置初始化级别
+      //   mapStyle: "amap://styles/57994c871bb604a4c79184f5f65d8782",
+      //   center: [107.605197, 37.596541],
+      //   events: {},
+    };
+  },
   methods: {
     gotoIndex() {
       this.$router.replace("/Home");
@@ -20,18 +119,26 @@ export default {
 };
 </script>
 <style lang="less" scoped>
-.survey {
-  width: 100%;
-  height: 100%;
-  background-image: url("../assets/images/survey.png");
-  background-size: 100% 100%;
-  background-repeat: no-repeat;
+@import "../assets/css/home/survey.css";
+</style>
+<style>
+.survey_content .el-card {
+  border: 2px solid #1a699f;
+  background-color: transparent;
+  box-shadow: 0 0 15px #437799 inset;
 }
-.survey_footer {
-  position: fixed;
-  bottom: 0;
-  left: 94%;
-  top: 96%;
-  font-size: 16px;
+.survey_content .el-card__header {
+  border-bottom: 2px solid #1a699f !important;
+  padding: 10px 25px;
+}
+.survey_content_left .el-card {
+  height: 31%;
+  width: 100%;
+  margin: 1% 0 3% 0;
+}
+.survey_content_middle_body .el-card {
+  height: 100%;
+  width: 31%;
+  margin: 0 1%;
 }
 </style>
