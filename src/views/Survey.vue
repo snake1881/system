c77c3d3bc8ad920147e2811c138c1cf2a2912cff
cwqz <xiaonifan@163.com>
2021-05-12 16:14:59 +0800

    系统概况页添加地图

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -35,29 +35,51 @@
       </div>
       <div class="survey_content_middle">
         <div class="survey_content_middle_map">
-          <img
+          <!-- <img
             src="../assets/images/block.png"
             style="width: 96%; height: 94%; margin: 2% 2%"
-          />
-          <!-- <el-amap
+          /> -->
+          <el-amap
             vid="amapPro"
             :amap-manager="amapManager"
             :mapStyle="mapStyle"
             :zoom="zoom"
             :center="center"
             :events="events"
-          /> -->
+            :plugin="plugin"
+            viewMode="3D"
+          >
+            <el-amap-marker
+              v-for="(marker, index) in markers1"
+              :vid="index"
+              :key="index"
+              :position="marker"
+            ></el-amap-marker>
+            <el-amap-info-window
+              :position="markers2.position"
+              :content="markers2.content"
+              :visible="markers2.visible"
+            >
+            </el-amap-info-window>
+            <el-amap-text
+              v-for="(text, index1) in markers2"
+              :text="text.content"
+              :offset="text.offset"
+              :vid="index1"
+              :key="index1"
+            ></el-amap-text>
+          </el-amap>
         </div>
         <div class="survey_content_middle_body">
           <el-card>
             <div slot="header">
-              <span class="card_header">含水监测分析</span>
+              <span class="card_header">注水井监测</span>
             </div>
             <img src="../assets/images/survey_four.png" class="card_image" />
           </el-card>
           <el-card>
             <div slot="header">
-              <span class="card_header">注水井报表</span>
+              <span class="card_header">油井监测</span>
             </div>
             <img src="../assets/images/survey_five.png" class="card_image" />
           </el-card>
@@ -87,7 +109,7 @@
         </el-card>
         <el-card>
           <div slot="header">
-            <span class="card_header">基础信息</span>
+            <span class="card_header">油水井概况</span>
           </div>
           <img
             src="../assets/images/survey_seven.png"
@@ -99,16 +121,63 @@
   </div>
 </template>
 <script>
-// import { AMapManager } from "vue-amap";
-// let amapManager = new AMapManager(); // 新建生成地图画布
+import { AMapManager } from "vue-amap";
+
+let amapManager = new AMapManager(); // 新建生成地图画布
 export default {
   data() {
     return {
-      //   amapManager,
-      //   zoom: 12, // 设置初始化级别
-      //   mapStyle: "amap://styles/57994c871bb604a4c79184f5f65d8782",
-      //   center: [107.605197, 37.596541],
-      //   events: {},
+      amapManager,
+      plugin: [
+        "ToolBar",
+        {
+          pName: "MapType",
+          defaultType: 1,
+        },
+      ],
+      zoom: 13, // 设置初始化级别
+      mapStyle: "amap://styles/57994c871bb604a4c79184f5f65d8782",
+      center: [107.789816, 37.513493],
+      events: {},
+      markers1: [
+        [107.8206307, 37.5193102],
+        [107.739519, 37.49868848],
+        [107.7346543, 37.50237174],
+        [107.7190803, 37.49956097],
+        [107.8428759, 37.53803695],
+      ],
+      markers2: [
+        {
+          position: [107.8206307, 37.5193102],
+          content: "定1656-3",
+          visible: true,
+          offset: [-240, 70],
+        },
+        {
+          position: [107.739519, 37.49868848],
+          content: "定1764-1",
+          visible: true,
+          offset: [-280, 33],
+        },
+        {
+          position: [107.7346543, 37.50237174],
+          content: "定1988-2",
+          visible: true,
+          offset: [-390, 120],
+        },
+        {
+          position: [107.7190803, 37.49956097],
+          content: "定1988B-10",
+          visible: true,
+          offset: [260, -160],
+        },
+        {
+          position: [107.8428759, 37.53803695],
+          content: "定1876-2",
+          visible: true,
+          offset: [130, -80],
+        },
+      ],
     };
   },
   methods: {
